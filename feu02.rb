b6f55f3e25313ce5005faa1980b360fdb6ad85bf
Akeem57 <lyrlandais@hotmail.com>
2023-09-26 18:23:58 +0200

    Preeeeesquegit add .!

--- feu02.rb
+++ feu02.rb
@@ -24,6 +24,71 @@ $> ruby exo.rb board.txt unfindable.txt
 Introuvable
 Vous devez gérer les potentiels problèmes d’arguments et de lecture de ﬁchiers.
 =end
+#result = []
 
+=begin
+File.open(ARGV[0], 'r') do |file|
+    line_index = 0
 
+    file.each_line do |ligne|
+      char_index = 0
+
+      ligne.chomp.each_char do |caractere|
+        result << [caractere, line_index, char_index]
+        char_index += 1
+      end
   
+      line_index += 1
+    end
+end
+  
+  result.each do |item|
+    print "#{item[0]} [#{item[1]}, #{item[2]}]"
+    puts
+=end
+
+    
+board = File.open(ARGV[0], "r").read.split(/\n/) # = ["0000", "1111", "2331"]
+target = File.open(ARGV[1], "r").read.split(/\n/) # = ["11", "1"]
+
+matches = board.grep(/#{target}/)
+
+if matches
+    puts "Trouvé!"
+else
+    puts "pas de correspondances"
+end
+
+
+i=0
+j=0
+k=0
+l=0
+result=[]
+x=0
+y=0
+while i < board.length
+    if board[i][j] == target[k][l] && board[i][j+1] == target[k][l+1] && ((board[i+1][j] == target[k+1][l]) || (board[i+1][j+1] == target[k+1][l]))
+        result << board[i][j]
+        x= j
+        y= i
+    else
+        result << "-"
+    end
+    j += 1
+    if j == board[i].length
+        j=0
+        i+=1
+        result << "\n"
+    end
+    if i == board.length
+        break
+    end
+
+end
+
+puts result.join("")
+
+puts "Coordonnées : #{x}, #{y}"
+
+
