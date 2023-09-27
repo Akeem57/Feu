4abde0ffc496f8c2f7cf84dbdc3948ff37ba242e
Akeem57 <lyrlandais@hotmail.com>
2023-09-27 21:01:46 +0200

    En cours

--- feu02.rb
+++ feu02.rb
@@ -10,7 +10,7 @@ $> cat board.txt
 2331
 $> cat to_find.txt
 11
-1
+ 1
 $> cat unfindable.txt
 00
 00
@@ -24,30 +24,8 @@ $> ruby exo.rb board.txt unfindable.txt
 Introuvable
 Vous devez gérer les potentiels problèmes d’arguments et de lecture de ﬁchiers.
 =end
-#result = []
 
-=begin
-File.open(ARGV[0], 'r') do |file|
-    line_index = 0
 
-    file.each_line do |ligne|
-      char_index = 0
-
-      ligne.chomp.each_char do |caractere|
-        result << [caractere, line_index, char_index]
-        char_index += 1
-      end
-  
-      line_index += 1
-    end
-end
-  
-  result.each do |item|
-    print "#{item[0]} [#{item[1]}, #{item[2]}]"
-    puts
-=end
-
-    
 board = File.open(ARGV[0], "r").read.split(/\n/) # = ["0000", "1111", "2331"]
 target = File.open(ARGV[1], "r").read.split(/\n/) # = ["11", "1"]
 
@@ -59,38 +37,32 @@ else
     puts "pas de correspondances"
 end
 
+board_final = []
 
-i=0
-j=0
-k=0
-l=0
-result=[]
-x=0
-y=0
-while i < board.length
-    if board[i][j] == target[k][l] && board[i][j+1] == target[k][l+1] && ((board[i+1][j] == target[k+1][l]) || (board[i+1][j+1] == target[k+1][l]))
-        result << board[i][j]
-        puts "Trouvé!"
-        x= j
-        y= i
-        puts "Coordonnées : #{x}, #{y}"
-    else
-        result << "-"
+board.each do |lign| # resultat : [[["0"], ["0"], ["0"], ["0"]], [["1"], ["1"], ["1"], ["1"]], [["2"], ["3"], ["3"], ["1"]]]
+    lign_transform = []
+    lign.each_char do |char|
+        lign_transform << [char]
     end
-    j += 1
-    if j == board[i].length
-        j=0
-        i+=1
-        result << "\n"
-    end
-    if i == board.length
-        break
-    end
-
+    board_final << lign_transform
 end
 
-puts result.join("")
+=begin             # => resultat : [["0"], ["0"], ["0"], ["0"], ["1"], ["1"], ["1"], ["1"], ["2"], ["3"], ["3"], ["1"]]
+board.each do |lign|  => donc osef
+    lign.each_char do |char| 
+        board_final << [char]
+    end
+end
+=end
 
-
-
+target_final = []  # => resultat : [[["1"], ["1"]], [["1"]]]
+target.each do |lign|
+    lign_transform = []
+    lign.each_char do |char|
+        lign_transform << [char]
+    end
+    target_final << lign_transform
+end
+  
+result = []
 
